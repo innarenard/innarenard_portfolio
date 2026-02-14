9fac8b0486d3744ca21b2d56d4c560190c7beb30
innarenard <innarenard@gmail.com>
2026-02-14 18:55:45 +0000

    Initial website version

--- README.md
+++
@@ -1,1 +0,0 @@
-# innarenard_portfolio
---
+++ js/nav.js
@@ -0,0 +1,34 @@
+(function () {
+  var nav = document.querySelector('.nav-menu-2');
+  var button = document.querySelector('.menu-button');
+
+  if (!nav || !button) return;
+
+  function toggleMenu() {
+    var isOpen = nav.hasAttribute('data-nav-menu-open');
+    if (isOpen) {
+      nav.removeAttribute('data-nav-menu-open');
+      button.classList.remove('w--open');
+    } else {
+      nav.setAttribute('data-nav-menu-open', '');
+      button.classList.add('w--open');
+    }
+  }
+
+  function closeMenu() {
+    nav.removeAttribute('data-nav-menu-open');
+    button.classList.remove('w--open');
+  }
+
+  button.addEventListener('click', toggleMenu);
+
+  nav.querySelectorAll('a').forEach(function (link) {
+    link.addEventListener('click', closeMenu);
+  });
+
+  document.addEventListener('click', function (e) {
+    if (nav.hasAttribute('data-nav-menu-open') && !nav.contains(e.target) && !button.contains(e.target)) {
+      closeMenu();
+    }
+  });
+})();
